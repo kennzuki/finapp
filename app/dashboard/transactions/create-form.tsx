2e6606fd40c3500914e6b28fce2daae02cef5fb5
kennzuki <kingsyken@gmail.com>
2025-01-29 18:15:47 +0300

    create transaction form

--- app/dashboard/transactions/create-form.tsx
+++ app/dashboard/transactions/create-form.tsx
@@ -2,24 +2,16 @@
 
 // import { Transactions } from '@/lib/definations';
 
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-
 export default function Form() {
   return (
     <>
-      <h1 className="dark:text-white text-2xl font-bold uppercase mb-12 text-center">Add a transaction</h1>
-      <form className='space-y-4 max-w-md mx-auto p-6 bg-white rounded-lg shadow-md'>
-        <Input type='date' name='date' className='w-full dark:bg-white' />
+      <h1 className='dark:text-white text-2xl font-bold uppercase mb-12 text-center'>
+        Add a transaction
+      </h1>
+      <form className='space-y-4 max-w-md mx-auto p-6 bg-white rounded-lg shadow-md h-1/2 w-1/2'>
+        <input type='date' name='date' className='w-full dark:bg-white' />
 
-        <Input
+        <input
           type='number'
           name='amount'
           placeholder='Amount'
@@ -27,27 +19,29 @@ export default function Form() {
           min='0'
           className='w-full dark:bg-white'
         />
+        <label>Choose a transaction type:</label>
+        <select name='category'>
+          <option value='income' className='w-full dark:bg-white text-gray-600'>
+            Income
+          </option>
+          <option value='debt' className='w-full dark:bg-white text-black'>
+            Debt
+          </option>
+        </select>
 
-        <Select name='category'  >
-          <SelectTrigger className='w-full dark:bg-white'>
-            <SelectValue placeholder='Select Category' />
-          </SelectTrigger>
-          <SelectContent className='w-full dark:bg-white'>
-            <SelectItem value='income'className='w-full dark:bg-white text-gray-600'>Income</SelectItem>
-            <SelectItem value='debt' className='w-full dark:bg-white text-black'>Debt</SelectItem>
-          </SelectContent>
-        </Select>
-
-        <Input
+        <input
           type='text'
           name='description'
           placeholder='Description'
           className='w-full dark:bg-white'
         />
 
-        <Button type='submit' className='w-full bg-black text-white font-bold'>
+        <button
+          type='submit'
+          className='w-full py-2 px-4 rounded-xl shadow-lg bg-black text-white font-bold'
+        >
           Add Transaction
-        </Button>
+        </button>
       </form>
     </>
   );
