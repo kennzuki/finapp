44a181787132f83350157f0fe30871a8141fb857
kennzuki <kingsyken@gmail.com>
2025-02-04 20:29:36 +0300

    update actions and button

--- app/dashboard/transactions/buttons.tsx
+++ app/dashboard/transactions/buttons.tsx
@@ -2,6 +2,19 @@
 
 import { deleteTransactions } from '@/lib/actions';
 import { FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import Link from 'next/link';
+
+export function UpdateInvoice({ id }: { id: string }) {
+    return (
+      <Link
+        href={`/dashboard/invoices/${id}/edit`}
+        className="rounded-md border p-2 hover:bg-gray-100"
+      >
+        <FaPencil className="w-5" />
+      </Link>
+    );
+  }
 
 export function DeleteTransaction({ id }: { id: number }) {
     const deleteTransactionWithId =  deleteTransactions.bind(null, id);
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { prisma } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function createTransactions(formData: FormData) {
   await prisma.expense.create({
@@ -11,8 +12,23 @@ export async function createTransactions(formData: FormData) {
     },
   });
     revalidatePath('/dashboard/transactions');
+    redirect('/dashboard/transactions');
+}
+
+export async function updateTransactions(id: number, formData: FormData) { 
+    await prisma.expense.update({
+      where: { id },
+      data: {
+        category: formData.get('category') as string,
+        amount: parseInt(formData.get('amount') as string),
+      },
+    });
+    revalidatePath('/dashboard/transactions');
+    redirect('/dashboard/transactions');
+ 
 }
 export async function deleteTransactions(id: number) {
     await prisma.expense.delete({ where: { id } });
     revalidatePath('/dashboard/transactions');
+    redirect('/dashboard/transactions');
  }
