6e88aca256e094712f254573ad2a89e0a8153278
kennzuki <kingsyken@gmail.com>
2025-02-14 00:44:08 +0300

    navbar for main page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+      <div>
+          <nav className="flex place-content-end p-6 bg-white shadow">
+              <button className="bg-blue-400 text-white py-2 px-4 font-bold rounded-xl">
+                  <Link href={'./Dashboard'}>Dashboard</Link>
+         </button>
+          </nav>
+    </div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "./components/Hero";
+import { Navbar } from "./components/Navbar";
 
 
 export default function Home() {
   return (
     <div className="bg-white text-black text-center p-12 space-y-4">
-     
+     <Navbar/>
       <Hero/>
     </div>
     
